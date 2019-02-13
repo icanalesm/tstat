fab8d58e54a5f3ed1491620f753a2839bd41d775
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2019-02-13 22:46:19 +0100

    Update volume function
    - Reorder variables for readability
    - Assign varible in declaration
    - Reorder allocation of sid and handler
    - Homogenize error messages
    - Add check for a by-zero division

--- tstat.c
+++ tstat.c
@@ -97,68 +97,69 @@ const char *datetime(const void *fmt)
 
 const char *volume(const void *mixer)
 {
-	int aux;
 	long vol, min, max;
+	int aux;
 	size_t i;
-	struct mixer *mix;
-	snd_mixer_t *handle;
-	snd_mixer_selem_id_t *sid;
-	snd_mixer_elem_t *elem;
 	char *s[] = { "Mute", "Unmute" };
+	struct mixer *mix = (struct mixer *) mixer;
+	snd_mixer_selem_id_t *sid;
+	snd_mixer_t *handle;
+	snd_mixer_elem_t *elem;
 
-	mix = (struct mixer *)mixer;
-
-	if ((aux = snd_mixer_open(&handle, 0)) < 0) {
-		error("Mixer open error: %s", snd_strerror(aux));
-		return NULL;
-	}
-	if ((aux = snd_mixer_attach(handle, mix->card)) < 0) {
-		error("Mixer attach '%s' error: %s", mix->card, snd_strerror(aux));
-		goto clean_handle;
-	}
-	if ((aux = snd_mixer_selem_register(handle, NULL, NULL)) < 0) {
-		error("Mixer register error: %s", snd_strerror(aux));
-		goto clean_handle;
-	}
- 	if ((aux = snd_mixer_load(handle)) < 0) {
-		error("Mixer load '%s' error: %s", mix->card, snd_strerror(aux));
-		goto clean_handle;
-	}
 	if ((aux = snd_mixer_selem_id_malloc(&sid)) < 0) {
 		error("Mixer allocation error: %s", snd_strerror(aux));
-		goto clean_handle;
+		return NULL;
 	}
 	snd_mixer_selem_id_set_index(sid, mix->index);
 	snd_mixer_selem_id_set_name(sid, mix->name);
-	if (!(elem = snd_mixer_find_selem(handle, sid))) {
-		error("Unable to find '%s',%i in card '%s'", mix->name,
-		      mix->index, mix->card);
+	if ((aux = snd_mixer_open(&handle, 0)) < 0) {
+		error("Mixer %s open error: %s", mix->card, snd_strerror(aux));
 		goto clean_sid;
 	}
+	if ((aux = snd_mixer_attach(handle, mix->card)) < 0) {
+		error("Mixer %s attach error: %s", mix->card, snd_strerror(aux));
+		goto clean_handle;
+	}
+	if ((aux = snd_mixer_selem_register(handle, NULL, NULL)) < 0) {
+		error("Mixer %s register error: %s", mix->card, snd_strerror(aux));
+		goto clean_handle;
+	}
+ 	if ((aux = snd_mixer_load(handle)) < 0) {
+		error("Mixer %s load error: %s", mix->card, snd_strerror(aux));
+		goto clean_handle;
+	}
+	if (!(elem = snd_mixer_find_selem(handle, sid))) {
+		error("Unable to find simple control %s,%i in card %s",
+		      mix->name, mix->index, mix->card);
+		goto clean_handle;
+	}
+
 	/* Get playback switch (0 = mute, 1 = unmute) */
 	if (snd_mixer_selem_has_playback_switch(elem) &&
 	    snd_mixer_selem_get_playback_switch(elem, SND_MIXER_SCHN_MONO,
 	                                        &aux) < 0) {
-		error("Unable to get switch status from '%s',%i in card '%s'",
+		error("Unable to get switch status from %s,%i in card %s",
 		      mix->name, mix->index, mix->card);
-		goto clean_sid;
+		goto clean_handle;
 	}
 	/* Get volume */
 	if (snd_mixer_selem_get_playback_volume_range(elem, &min, &max) < 0 ||
 	    snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_MONO,
 	                                        &vol) < 0) {
-		error("Unable to get volume from '%s',%i in card '%s'",
+		error("Unable to get volume from %s,%i in card %s",
 		      mix->name, mix->index, mix->card);
-		goto clean_sid;
+		goto clean_handle;
         }
-	/* Clean */
-	snd_mixer_selem_id_free(sid);
+
 	snd_mixer_close(handle);
+	snd_mixer_selem_id_free(sid);
 
 	/* Normalise volume and compute percentage */
 	vol -= min;
 	max -= min;
-	vol = 100 * vol / max;
+	if (max == 0)
+		return NULL;
+	vol = (long) ((double) vol / (double) max * 100);
 
 	for (i = 0; i < LEN(volst); i++) {
 		if (!strcmp(volst[i].state, s[aux]) && vol <= volst[i].perc) {
@@ -167,10 +168,10 @@ const char *volume(const void *mixer)
 		}
 	}
 
-clean_sid:
-	snd_mixer_selem_id_free(sid);
 clean_handle:
 	snd_mixer_close(handle);
+clean_sid:
+	snd_mixer_selem_id_free(sid);
 
 	return NULL;
 }
