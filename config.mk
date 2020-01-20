42041544465f633bc763d725b4c840f48c934c9b
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2020-01-20 09:36:21 +0100

    Remove spaces

--- config.mk
+++ config.mk
@@ -4,10 +4,10 @@
 PREFIX = /usr/local
 
 # flags
-CFLAGS   = -std=iso9899:1999 -Wpedantic -Wall -Wextra -O2
+CFLAGS = -std=iso9899:1999 -Wpedantic -Wall -Wextra -O2
 CPPFLAGS =
-LDFLAGS  =
-LDLIBS   = -lasound -lX11
+LDFLAGS =
+LDLIBS = -lasound -lX11
 
 # compiler
 CC = gcc
