944eccacef6efc41c8253760ea93f41b30daf2dd
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2025-03-04 12:30:10 +0400

    Refactor and document util.{h,c}

--- util.c
+++ util.c
@@ -7,51 +7,50 @@
 
 void error(const char *fmt, ...)
 {
-	va_list va;
+        va_list va;
 
-	va_start(va, fmt);
-	fprintf(stderr, "tstat: ");
-	vfprintf(stderr, fmt, va);
-	fprintf(stderr, "\n");
-	va_end(va);
+        va_start(va, fmt);
+        fprintf(stderr, "tstat: ");
+        vfprintf(stderr, fmt, va);
+        fprintf(stderr, "\n");
+        va_end(va);
 }
 
 int esnprintf(char *buffer, size_t bfsz, const char *fmt, ...)
 {
-	va_list va;
-	int ret;
+        va_list va;
+        int ret;
 
-	va_start(va, fmt);
-	ret = vsnprintf(buffer, bfsz, fmt, va);
-	va_end(va);
-	
-	if (ret < 0)
-		error("esnprintf: %s", strerror(errno));
-	else if ((size_t)ret >= bfsz)
-		error("esnprintf: Output truncated");
+        va_start(va, fmt);
+        ret = vsnprintf(buffer, bfsz, fmt, va);
+        va_end(va);
+        
+        if (ret < 0)
+                error("esnprintf: %s", strerror(errno));
+        else if ((size_t)ret >= bfsz)
+                error("esnprintf: Output truncated");
 
-	return ret;
+        return ret;
 }
 
-int epscanf(const char *path, const char *fmt, ...)
+int epscanf(const char *filename, const char *fmt, ...)
 {
-	FILE *f;
-	va_list va;
-	int ret;
+        FILE *f;
+        va_list va;
+        int ret;
 
-	if (!(f = fopen(path, "r"))) {
-		error("epscanf '%s': %s", path, strerror(errno));
-		return EOF;
-	}
+        if (!(f = fopen(filename, "r"))) {
+                error("epscanf '%s': %s", filename, strerror(errno));
+                return EOF;
+        }
 
-	va_start(va, fmt);
-	ret = vfscanf(f, fmt, va);
-	va_end(va);
-	fclose(f);
+        va_start(va, fmt);
+        ret = vfscanf(f, fmt, va);
+        va_end(va);
+        fclose(f);
 
-	if (ret < 0)
-		error("epscanf: Error while assigning values");
+        if (ret == EOF)
+                error("epscanf: Error while assigning values");
 
-	return ret;
+        return ret;
 }
-
--- util.h
+++ util.h
@@ -1,10 +1,58 @@
 #include <stddef.h>
 
-#define LEN(X)  (sizeof(X) / sizeof((X)[0]))                                          
+#define LEN(X)  (sizeof(X) / sizeof((X)[0]))
 
+/**
+ * Writes to stderr prefixing the data with "tstat: " and appending a new line
+ * character.
+ *
+ * @param fmt Pointer to a null-terminated character string specifying how to
+ *            interpret the data.
+ * @param ... Arguments specifying data to print.
+ */
 void error(const char *fmt, ...);
 
+/**
+ * Loads data from the given locations, converts them to character string
+ * equivalents and writes the results to the character string buffer.
+ *
+ * At most bfsz - 1 characters are written. The resulting character string will
+ * be terminated with a null character, unless bfsz is zero. If bfsz is zero,
+ * nothing is written and buffer may be a null pointer, however the return value
+ * (number of bytes that would be written not including the null terminator) is
+ * still calculated and returned.
+ *
+ * This function calls error if an error occurs or if the number of characters
+ * written was truncated due to the bfsz limit.
+ *
+ * @param buffer Pointer to a character string to write to.
+ * @param bfsz Limit on the number of characters that may be written, including
+ *             the null terminator.
+ * @param fmt Pointer to a null-terminated character string specifying how to
+ *            interpret the data.
+ * @param ... Arguments specifying data to print.
+ * @return The number of characters written if successful or a negative value if
+ *         an error occurred. If the resulting string gets truncated due to the
+ *         bfsz limit, the function returns the total number of characters (not
+ *         including the terminating null-byte) which would have been written,
+ *         if the limit was not imposed.
+ */
 int esnprintf(char *buffer, size_t bfsz, const char *fmt, ...);
 
-int epscanf(const char *path, const char *fmt, ...);
-
+/**
+ * Reads data from the file indicated by filename, interprets it according to
+ * the given format string and stores the results into the given locations.
+ *
+ * This function calls error if an error occurs when opening the file or if read
+ * failure occurs before the first receiving argument was assigned.
+ *
+ * @param filename File name to read from.
+ * @param fmt Pointer to a null-terminated character string specifying how to
+ *            read the input.
+ * @param ... Receiving arguments.
+ * @return Number of receiving arguments successfully assigned, or EOF if
+ *   - an error occurs while opening the file, or
+ *   - a read failure occurs before the first receiving argument was assigned.
+ * @see error()
+ */
+int epscanf(const char *filename, const char *fmt, ...);
